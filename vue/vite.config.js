224c933ded6d2638a929227e98456bdc322c9b8e
Lamaxime <lamaxime20@gmail.com>
2025-10-31 01:20:14 +0100

    Autorisation vite

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: ['miam-miam-shop.onrender.com']
   }
 })
